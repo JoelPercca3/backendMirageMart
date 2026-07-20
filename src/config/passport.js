9c3e0060c9fcbcdb566b1561f5cf6563941eed1e
Joel Percca Cuadros <joelpercca3@gmail.com>
2026-07-19 23:25:01 -0500

    passorp nuevo para railway

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,13 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import {
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL,
-} from "./env.js";
 import { findOrCreateGoogleUser } from "../services/auth.service.js";
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: GOOGLE_CALLBACK_URL,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
