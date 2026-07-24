b38b269e84b55ddc3088724f2c78e33638befa3b
Joel Percca Cuadros <joelpercca3@gmail.com>
2026-07-23 20:54:58 -0500

    solicutdes limitecambiado

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -2,12 +2,12 @@ import rateLimit from "express-rate-limit";
 
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
     ok: false,
-    message: "Demasiadas solicitudes. Intenta nuevamente en 15 minutos.",
+    message: "Demasiadas solicitudes. Intenta nuevamente en unos minutos.",
   },
 });
 
